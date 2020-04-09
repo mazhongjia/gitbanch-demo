525108f2dd33961d7c6b545812a6e9a6036726ef
mazhongjia <mazhongjia126@126.com>
2020-04-09 17:10:04 +0800

    初版提交

---
+++ src/main/java/com/mzj/gitbanch/App.java
@@ -0,0 +1,13 @@
+package com.mzj.gitbanch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
